3ff8a794bd9e6c8a496b2841df05668e5abf3dea
priyamishra2903 <priya.1721ec1106@kiet.edu>
2020-12-10 22:31:37 +0530

    update

--- src/components/services/services.js
+++ src/components/services/services.js
@@ -16,21 +16,21 @@ function Services() {
                     <h3 className="h4 mb-2">Front End Development
 
                     </h3>
-                    <p className="text-muted mb-0">Developing data into a graphical interface through the use of HTML, CSS, and JavaScript.</p>
+                    <p className="text-muted mb-0">Designing responsive web pages using, HTML, CSS, JavaScript and Reactjs.</p>
                 </div>
             </div>
             <div className="col-lg-4 col-md-6 text-center">
                 <div className="mt-5">
                     <img src="./assets/img/portfolio/thumbnails/icons-03.png" alt="icons" className="responsive"/>
-                    <h3 className="h4 mb-2">Database</h3>
-                    <p className="text-muted mb-0">Creating products that provide meaningful and relevant experiences to users. This involves integration of design & usability.</p>
+                    <h3 className="h4 mb-2">Backend Development</h3>
+                    <p className="text-muted mb-0"> Writing REST api using Nodejs, Express(Framework).</p>
                 </div>
             </div>
             <div className="col-lg-4 col-md-6 text-center">
                 <div className="mt-5">
                     <img src="./assets/img/portfolio/thumbnails/icons-02.png" alt="icons" className="responsive"/>
-                    <h3 className="h4 mb-2">Back End Development</h3>
-                    <p className="text-muted mb-0">Developing server side applications and everything that communicates between the database and the browser.</p>
+                    <h3 className="h4 mb-2">Database</h3>
+                    <p className="text-muted mb-0"> Working on MySql Server</p>
                 </div>
             </div>
             </div>
